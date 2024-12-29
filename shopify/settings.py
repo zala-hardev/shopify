4ec389bead4e6fd9d5d2289545f5265f8ebec1f8
zala-hardev <zala07754@gmail.com>
2024-12-29 15:38:09 +0530

    Update settings.py

--- shopify/settings.py
+++ shopify/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-qoep*%v06sv$i7j7%v4h*givyiwju$wr*x_v3*wswk4-i&241*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.vercel.app','.now.sh']
 
